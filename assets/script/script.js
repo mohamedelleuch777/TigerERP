23f0ac91794cd53a083d247629684c34d4e9927b
mohamedelleuch777 <usher7med@gmail.com>
2022-12-06 03:21:01 +0300

    first api implemented

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,14 +2,14 @@ $(document).ready(function(){
 
     // click on login button in the login page
     $('#btn-submit-login').click(async function(){
-        var email = $($('input[type=email]')[0]).val()
+        var username = $($('input[name=username]')[0]).val()
         var password = $($('input[type=password]')[0]).val()
-        var result = await API_Login(email, password);
+        var result = await API_Login(username, password);
         if(result.success) {
             window.location.href = "/dashborad.html";
         } else {
             var errorField = $($('#error-field')[0])
-            errorField.text(result.error);
+            errorField.text(result.message);
             errorField.show();
         }
     });
@@ -18,6 +18,7 @@ $(document).ready(function(){
     $('#btn-refister').click(async function(){
         var fName = $($('input[name=first-name]')[0]).val()
         var lName = $($('input[name=last-name]')[0]).val()
+        var username = $($('input[name=username]')[0]).val()
         var email = $($('input[type=email]')[0]).val()
         var password = $($('input[name=password]')[0]).val()
         var confirmation = $($('input[name=confirmation]')[0]).val();
@@ -27,12 +28,12 @@ $(document).ready(function(){
             errorField.show();
             return;
         }
-        var result = await API_Register(email, password, fName, lName);
+        var result = await API_Register(username, email, password, fName, lName);
         if(result.success) {
             window.location.href = "/dashborad.html";
         } else {
             var errorField = $($('#error-field')[0])
-            errorField.text(result.error);
+            errorField.text(result.message);
             errorField.show();
         }
     });
@@ -69,15 +70,29 @@ $(document).ready(function(){
 });
 
 
-function API_Login(email, password) {
-    return {
-        success: false,
-        error: "Invalid credentials"
+async function API_Login(username, password) {
+    let data = {
+        username: username,
+        password: password
     }
+    let result = await fetch("http://localhost:5000/user", {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+      })
+    return await result.json();
 }
 
 
-function API_Register(email, password, firstName, lastName) {
+function API_Register(username, email, password, firstName, lastName) {
     return {
         success: false,
         error: "This email ("+email+") aleady exisit in out database"
